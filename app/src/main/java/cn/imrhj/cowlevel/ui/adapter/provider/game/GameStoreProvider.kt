7f75b1784637cc6752efd81fab9ae0424ead40f1
任洪建 <hexrhj@gmail.com>
2018-10-16 18:57:23 +0800

    购买与下载

--- app/src/main/java/cn/imrhj/cowlevel/ui/adapter/provider/game/GameStoreProvider.kt
+++ app/src/main/java/cn/imrhj/cowlevel/ui/adapter/provider/game/GameStoreProvider.kt
@@ -1,9 +1,16 @@
 package cn.imrhj.cowlevel.ui.adapter.provider.game
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import cn.imrhj.cowlevel.R
 import cn.imrhj.cowlevel.consts.ItemTypeEnum
 import cn.imrhj.cowlevel.network.model.common.UrlListModel
+import cn.imrhj.cowlevel.utils.cdnImageForDPSquare
+import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseViewHolder
 import com.chad.library.adapter.base.provider.BaseItemProvider
 
@@ -16,9 +23,25 @@ class GameStoreProvider : BaseItemProvider<UrlListModel, BaseViewHolder>() {
         return ItemTypeEnum.TYPE_URLS.ordinal
     }
 
+    @SuppressLint("SetTextI18n")
     override fun convert(helper: BaseViewHolder, data: UrlListModel, position: Int) {
         helper.setText(R.id.tv_title, "购买与下载")
         val parent = helper.getView<LinearLayout>(R.id.ll_store)
+        data.list.forEach {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_game_store_row, parent, false)
+            itemView.findViewById<TextView>(R.id.tv_name).text = it?.name
+            val imageView = itemView.findViewById<ImageView>(R.id.iv_store_tag)
+            Glide.with(parent.context)
+                    .load(cdnImageForDPSquare(it?.pic, 28))
+                    .into(imageView)
+            if (it?.data?.cnyPrice?.length ?: 0 > 0) {
+                val priceView = itemView.findViewById<TextView>(R.id.tv_price)
+                priceView.visibility = View.VISIBLE
+                priceView.text = "¥ ${it?.data?.cnyPrice}"
+            }
+            itemView.setOnClickListener { }
+            parent.addView(itemView)
+        }
 
     }
 }

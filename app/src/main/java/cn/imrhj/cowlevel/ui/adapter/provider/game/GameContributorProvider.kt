a5b96bd1429d7abddcef976bd8f08518dce03394
任洪建 <hexrhj@gmail.com>
2018-10-19 18:12:01 +0800

    调整跳转

--- app/src/main/java/cn/imrhj/cowlevel/ui/adapter/provider/game/GameContributorProvider.kt
+++ app/src/main/java/cn/imrhj/cowlevel/ui/adapter/provider/game/GameContributorProvider.kt
@@ -1,7 +1,11 @@
 package cn.imrhj.cowlevel.ui.adapter.provider.game
 
 import android.content.Context
+import android.os.Build
+import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.util.Pair
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -11,6 +15,7 @@ import cn.imrhj.cowlevel.extensions.gotColor
 import cn.imrhj.cowlevel.manager.SchemeUtils
 import cn.imrhj.cowlevel.network.model.common.NameIDModel
 import cn.imrhj.cowlevel.network.model.game.PublishContributorsModel
+import cn.imrhj.cowlevel.ui.activity.PersonActivity
 import cn.imrhj.cowlevel.utils.ScreenSizeUtil.dp2px
 import cn.imrhj.cowlevel.utils.cdnImageForDPSquare
 import com.bumptech.glide.Glide
@@ -52,13 +57,25 @@ class GameContributorProvider : BaseItemProvider<PublishContributorsModel, BaseV
         data?.gameContributors?.forEach {
             val context = contributorParent.context
             val child = ImageView(context)
-            val params = LinearLayout.LayoutParams(dp2px(26), dp2px(26))
+            val params = LinearLayout.LayoutParams(dp2px(48), dp2px(48))
             params.marginEnd = dp2px(8)
             contributorParent.addView(child, params)
             Glide.with(child)
-                    .load(cdnImageForDPSquare(it?.avatar, 26))
+                    .load(cdnImageForDPSquare(it?.avatar, 48))
                     .apply(RequestOptions().circleCrop().placeholder(R.drawable.round_place_holder))
                     .into(child)
+            child.setOnClickListener { _ ->
+                val bundle = Bundle()
+                bundle.putString("avatar", it?.avatar)
+                bundle.putString("name", it?.name)
+                bundle.putString("url_slug", it?.urlSlug)
+                child.transitionName = "avatar"
+                SchemeUtils.startActivityTransition(PersonActivity::class.java, bundle,
+                        Pair.create(child as View, "avatar"))
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                child.foreground = context.getDrawable(R.drawable.card_foreground)
+            }
         }
     }
 }

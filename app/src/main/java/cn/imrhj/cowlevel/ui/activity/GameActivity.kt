1c5a34373c1ac0bf8bc14befffde38e2adbcbde9
任洪建 <hexrhj@gmail.com>
2018-10-20 10:59:41 +0800

    fix

--- app/src/main/java/cn/imrhj/cowlevel/ui/activity/GameActivity.kt
+++ app/src/main/java/cn/imrhj/cowlevel/ui/activity/GameActivity.kt
@@ -29,6 +29,7 @@ import com.elvishew.xlog.XLog
 import kotlinx.android.synthetic.main.activity_game.*
 
 
+
 @AppDeepLink("/game/{$KEY_URL_SLUG}")
 @WebDeepLink("/game/{$KEY_URL_SLUG}")
 class GameActivity : BaseActivity() {
